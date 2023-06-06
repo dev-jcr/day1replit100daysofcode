a9098184d01d25b6e529fc70428b30ab8e6cfa99
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 20:44:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print ("Hello Replit")
+print ("and how are you?")
+print ("emoticon")
+print ("""
+
+Here's a longer text
+Texting
+
+""")
+
+print ("Jonathan Cardona Rojas")
+print ("""
+03/14/2023
+""")
+print ("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling 😋
+You can follow my progress at replit.com/@threeyedinosaur
+""")
